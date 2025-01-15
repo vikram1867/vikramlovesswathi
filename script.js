13ce34c417e475a36986d9787dbd9d3070a9156c
vikram1867 <vguruswamy007@gmail.com>
2025-01-15 13:13:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,8 +35,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 300);
   });
 
+   const start = () => {
+            setTimeout(function() {
+                confetti.start()
+            }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+        };
+
+        //  for stopping the confetti 
+
+        const stop = () => {
+            setTimeout(function() {
+                confetti.stop()
+            }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
+        };
+  
   //"Yes" button
-  yesButton.addEventListener("click", function () {                     
+  yesButton.addEventListener("click", function () {     
+    start()
     alert("Yay Swathi! I love you :) 💖");
     window.location.href = 'loveletter.html';
   });
